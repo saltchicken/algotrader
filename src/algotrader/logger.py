51e561ac6b1b49913d524a1c395a11024fb18734
saltchicken <john.eicher89@gmail.com>
2026-03-01 18:31:14 -0800

    Remove timestamp of logger

--- src/algotrader/logger.py
+++ src/algotrader/logger.py
@@ -4,7 +4,8 @@ import logging
 def setup_logging(level=logging.INFO):
     logging.basicConfig(
         level=level,
-        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        # format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        format="[%(levelname)s] %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
 
